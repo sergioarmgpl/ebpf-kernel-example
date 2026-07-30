cf45cd2d2f8e615a3b83602ac7132a8475cb8af4
Sergio Méndez <sergioarm.gpl@gmail.com>
2026-07-30 11:30:54 -0600

    feat: updated exec.py

--- code/python/exec.py
+++ code/python/exec.py
@@ -1,5 +1,19 @@
 from bcc import BPF
-BPF(text='int kprobe__sys_clone(void *ctx) { bpf_trace_printk("Hello, World!\\n"); return 0; }').trace_print()
 
+# eBPF program written in C, it runs inside the kernel
+program = r"""
+int hello(void *ctx) {
+    bpf_trace_printk("Hello World from eBPF!\n");
+    return 0;
+}
+"""
 
-# Captures the output of the BPF program and prints it to the console. The BPF program is attached to the `sys_clone` kernel function, and whenever a process is cloned, it will print "Hello, World!" to the trace pipe.
+# Compile the C code into eBPF bytecode and load it into the kernel
+b = BPF(text=program)
+
+# Attach our function to the execve syscall using a kprobe
+syscall = b.get_syscall_fnname("execve")
+b.attach_kprobe(event=syscall, fn_name="hello")
+
+print("Tracing execve()... Press Ctrl+C to stop.")
+b.trace_print()
